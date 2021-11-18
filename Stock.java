fc90dde5d88b557c47e942f1ab323de0b65aaa14
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-18 10:04:05 +0530

    START :- Displayed Welcome message.

---
+++ Stock.java
@@ -0,0 +1,7 @@
+package com.Stock_Account_Management;
+
+public class Stock {
+	public static void main(String[] args) {
+		System.out.println("------------WELCOME TO STOCK ACCOUNT MANAGEMENT--------------");
+	}
+}
